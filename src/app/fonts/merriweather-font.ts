009c9f2dd033ed440335de620168e7057371f971
Andreev.TA <timoha-vdk@yandex.ru>
2025-01-06 12:20:41 +1000

    #2 Добавлены шрифты и storybook

---
+++ src/app/fonts/merriweather-font.ts
@@ -0,0 +1,30 @@
+import localFont from "next/font/local";
+
+const merriweatherFont = localFont({
+    src: [
+        {
+            path: './Merriweather/Merriweather-Regular.ttf',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: './Merriweather/Merriweather-Italic.ttf',
+            weight: '400',
+            style: 'italic',
+        },
+        {
+            path: './Merriweather/Merriweather-Bold.ttf',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path: './Merriweather/Merriweather-BoldItalic.ttf',
+            weight: '700',
+            style: 'italic',
+        },
+    ],
+    display: 'swap',
+    variable: '--merriweather-font',
+});
+
+export default merriweatherFont;
